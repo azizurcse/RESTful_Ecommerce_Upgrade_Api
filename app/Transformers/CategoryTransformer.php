a6b6ce1b47cba90101068c48c6349efd228cf057
azizurcse <azizur.jibon@gmail.com>
2018-03-25 12:05:58 +0600

    hateoas link with all model relationship

--- app/Transformers/CategoryTransformer.php
+++ app/Transformers/CategoryTransformer.php
@@ -21,7 +21,30 @@ class CategoryTransformer extends TransformerAbstract
             'creationDate'=>(string)$category->created_at,
             'lastDate'=>(string)$category->updated_at,
             'deletedDate'=>isset($category->deleted_at) ? (string) $category->deleted_at : null,
-        ];
+
+            'links'=>[
+                [
+                    'rel'=>'self',
+                    'href'=>route('categories.show',$category->id),
+                ],
+                [
+                    'rel'=>'category.buyers',
+                    'href'=>route('categories.buyers.index',$category->id),
+                ],
+                [
+                    'rel'=>'category.products',
+                    'href'=>route('categories.products.index',$category->id),
+                ],
+                [
+                    'rel'=>'category.sellers',
+                    'href'=>route('categories.sellers.index',$category->id),
+                ],
+                [
+                    'rel'=>'category.transactions',
+                    'href'=>route('categories.transactions.index',$category->id),
+                ]
+            ]
+        ];  
     }
 
    public static function originalAttribute($index)
