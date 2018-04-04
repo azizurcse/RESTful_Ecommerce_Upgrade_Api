c08eefbece157d8874b023d16bd7e754f1b8b57f
azizurcse <azizur.jibon@gmail.com>
2018-04-04 12:25:49 +0600

    add grant type authorization token generate

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -18,6 +18,14 @@ window.Vue = require('vue');
 Vue.component(
     'passport-personal-access-tokens', 
     require('./components/passport/PersonalAccessTokens.vue'));
+
+    Vue.component(
+        'passport-clients', 
+        require('./components/passport/Clients.vue'));
+
+        Vue.component(
+            'passport-authorized-clients', 
+            require('./components/passport/AuthorizedClients.vue'));
 // test
 const app = new Vue({
     el: '#app'
