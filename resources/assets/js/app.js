f8c05dc5fd2f4e6fac554b86969c595dbf1f2a21
azizurcse <azizur.jibon@gmail.com>
2018-04-04 13:33:30 +0600

    grant type authorization with uri_ decode section

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -19,13 +19,13 @@ Vue.component(
     'passport-personal-access-tokens', 
     require('./components/passport/PersonalAccessTokens.vue'));
 
-    Vue.component(
-        'passport-clients', 
-        require('./components/passport/Clients.vue'));
+Vue.component(
+     'passport-clients', 
+     require('./components/passport/Clients.vue'));
 
-        Vue.component(
-            'passport-authorized-clients', 
-            require('./components/passport/AuthorizedClients.vue'));
+ Vue.component(
+   'passport-authorized-clients', 
+    require('./components/passport/AuthorizedClients.vue'));
 // test
 const app = new Vue({
     el: '#app'
